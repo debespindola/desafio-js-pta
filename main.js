c801db76c33bd18932e1425f90a641dd65ea2e01
unknown <deborah.espindola@hotmail.com>
2020-06-28 12:25:19 -0300

    mudando a direção do movimento

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const paul = document.querySelector('.character');
+let direction = 1;
 
 const paulPosition = () => {
   const leftPosition = getComputedStyle(paul).left;
   let leftArray = Number(leftPosition.split('px').join(''));
-  leftArray += 5;
-  paul.style.left = `${leftArray}px`
+  leftArray += 5 * direction;
+  paul.style.left = `${leftArray}px`;
 }
 
-// setInterval(paulPosition, 20) adicionando movimento
+//------- adicionando movimento ----------------
+setInterval(paulPosition, 10); 
+
+//------------ mudando a direção do movimento-----------
+document.addEventListener('keypress', (event) => {
+  direction *= -1;
+}); 
