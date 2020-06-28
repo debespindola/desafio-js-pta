9ce7634fba361cfaefb9ba710c7d8d62514c97e5
unknown <deborah.espindola@hotmail.com>
2020-06-28 19:55:53 -0300

    colisao do personagem com as pedras e personagem olhando para a direcao

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const paul = document.querySelector('.character');
 const paulWidth = Number(getComputedStyle(paul).width.split('px').join(''));
+const paulHeight = Number(getComputedStyle(paul).height.split('px').join(''));
 
 const bodyWidth = getComputedStyle(document.body).width;
 const newBodyWidth = Number(bodyWidth.split('px').join(''));
@@ -19,16 +20,33 @@ const paulPosition = () => {
   const rocks = document.querySelectorAll('.rock');
 
   const leftPosition = getComputedStyle(paul).left;
-  let newLeft = Number(leftPosition.split('px').join(''));
-  newLeft += 8 * direction;
+  let leftPaul = Number(leftPosition.split('px').join(''));
+  leftPaul += 8 * direction;
   
-  if(!(newLeft < 0) && !((newLeft + paulWidth) > newBodyWidth)) {
-    paul.style.left = `${newLeft}px`;
+  if(!(leftPaul < 0) && !((leftPaul + paulWidth) > newBodyWidth)) {
+    paul.style.left = `${leftPaul}px`;
   }
   [...rocks].forEach((rock) => {
       let topRock = Number(getComputedStyle(rock).top.split('px').join(''));
       topRock += 10;
       rock.style.top = `${topRock}px`
+
+      const bottomRock = Number(getComputedStyle(rock).bottom.split('px').join(''));
+      const leftRock = Number(getComputedStyle(rock).left.split('px').join(''));
+      const widthRock = Number(getComputedStyle(rock).width.split('px').join(''));
+      const rightRock = Number(getComputedStyle(rock).right.split('px').join(''));
+      const rightPaul = Number(getComputedStyle(paul).right.split('px').join(''));
+
+      if(bottomRock <= paulHeight && bottomRock > 0) {
+        if(
+          !(leftPaul > (leftRock + widthRock))
+          && !(rightPaul > (rightRock + widthRock))
+        ){
+          clearInterval(game);
+          alert('GAME OVER');
+          window.location = window.location;
+        }
+      }
   })
 
   counter += 1;
@@ -38,14 +56,12 @@ const paulPosition = () => {
 }
 
 //----------- adicionando movimento --------------------
-setInterval(paulPosition, 1); 
+const game = setInterval(paulPosition, 1); 
 
 //------------ mudando a direção do movimento ----------
 document.addEventListener('keypress', (event) => {
   direction *= -1;
+  paul.style.transform = `scaleX(${-direction})`;
 }); 
 
-//------------ cair aleaoriamente -------------------
-// oq  eu quero fazer? eu quero que para cada nova pedra, sua posição horizontal mude
-// para isso, eu posso aumentar de forma aleatoria a distância entre a parede esquerda e a pedra em si
-// para isso, posso tentar usar a mesma propriedade que usei em paul, mas usando math.random
+
