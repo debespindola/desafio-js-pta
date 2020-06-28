a770db26fd2ffcbfc989bb9396102c653ca1cf35
unknown <deborah.espindola@hotmail.com>
2020-06-28 12:08:56 -0300

    adicionando movimento

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const paul = document.querySelector('.character');
 
 const paulPosition = () => {
   const leftPosition = getComputedStyle(paul).left;
-  console.log(leftPosition);
+  let leftArray = Number(leftPosition.split('px').join(''));
+  leftArray += 5;
+  paul.style.left = `${leftArray}px`
 }
 
+// setInterval(paulPosition, 20) adicionando movimento
